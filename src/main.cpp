afd9f29d4e7c17457e36454eca94114fd8ad76d9
Qtekfun <qtekfun@gmail.com>
2022-04-22 09:33:15 +0200

    feat: struct is RAII

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,45 @@ extern "C" {
     #include <tinyPTC/src/tinyptc.h>
 }
 #include <cstdint>
+#include <iostream>
 
-u_int32_t screen[640*360];
+constexpr uint32_t kR = 0x00FF00000;
+constexpr uint32_t kG = 0x0000FF000;
+constexpr uint32_t kB = 0x0000000FF;
 
-u_int32_t sprite[4*4] = {
-    0x00FF00000, 0x0000FF000, 0x0000000FF, 0x00FF00000,
-    0x00FF00000, 0x0000FF000, 0x0000000FF, 0x00FF00000,
-    0x00FF00000, 0x0000FF000, 0x0000000FF, 0x00FF00000,
-    0x00FF00000, 0x0000FF000, 0x0000000FF, 0x00FF00000
+constexpr u_int32_t sprite[8*8] = {
+    kR, kG, kB, kR,
+    kR, kG, kB, kR,
+    kR, kG, kB, kR,
+    kR, kG, kB, kR
 };
 
+constexpr uint32_t kSCRWIDTH  { 640 };
+constexpr uint32_t kSCRHEIGHT { 360 };
 
-int main(){
-    ptc_open("window", 640, 360);
+struct Screen_t {
+
+    Screen_t(uint32_t w, uint32_t h) : screen(new uint32_t[w*h]) {}
+    ~Screen_t() {
+        std::cout << "Paso? \n";
+        delete [] screen;
+    }
+
+    uint32_t * screen {nullptr};
+};
+
+void execute(){
+    ptc_open("window", kSCRWIDTH, kSCRHEIGHT);
+
+    Screen_t scr (kSCRWIDTH, kSCRHEIGHT);
 
     for (;;) {
         for (u_int32_t i=0; i < 640*360; ++i) {
-            screen[i] = 0x00FF00000;
+            scr.screen[i] = kR;
         }
         
-        u_int32_t *pscr = screen;
-        u_int32_t *psprt = sprite;
+        u_int32_t *pscr = scr.screen;
+        const u_int32_t *psprt = sprite;
 
         for (u_int32_t i=0; i < 4; ++i) {
             for (u_int32_t j=0; j < 4; ++j) {
@@ -33,10 +51,27 @@ int main(){
             pscr += 640 - 4;
         }
         
-        ptc_update(screen);
+        throw "Exception";
+
+        ptc_update(scr.screen);
     }
     
     ptc_close();
+}
+
+int main(){
+    try{
+        execute();
+    }
+    catch(std::exception& e){
+        std::cout << "Capturada\n";
+    }
+    catch(const char * s){
+        std::cout << "Capturada: " << s << "\n";
+    }
+    catch(...){
+        std::cout << "Capturada default: \n";
+    }
 
     return 0;
 }
